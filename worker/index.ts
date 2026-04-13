ec00f04b9047473c6b1c020f1829dfe94153a456
kylixmedusa <aayush.agarwal@medable.com>
2026-04-13 07:41:18 +0530

    Move IPv4 DNS fix to worker entry point for early execution

--- worker/index.ts
+++ worker/index.ts
@@ -1,3 +1,6 @@
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import 'dotenv/config';
 import { startJobProcessor, gracefulShutdown } from './processor';
 import { pool } from '../shared/db';
